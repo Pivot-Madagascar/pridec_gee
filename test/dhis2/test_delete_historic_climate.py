4f1b41f99f9ca532a4d2471efccb4b92d0aa74e6
mevans <mv.evans.phd@gmail.com>
2026-04-20 12:54:44 +0200

    #4 pytest architecture and github action testing on PR

--- test/dhis2/test_delete_historic_climate.py
+++ test/dhis2/test_delete_historic_climate.py
@@ -1,4 +1,5 @@
 from pridec_gee import delete_historic_climate
+import pytest
 
 debug=False
 if debug:
@@ -20,3 +21,25 @@ def test_delete_historic_climate_connects(dhis_url, dhis_token, api_connection):
     assert resp.ok
 
 #should write a function to ensure it actually deletes too
+
+def test_delete_historic_error_noUser(dhis_url):
+    with pytest.raises(ValueError):
+        delete_historic_climate(base_url=dhis_url,
+                            user=None,
+                            pwd="secret",
+                            orgUnit_ids=["O1wNJut8eci"],
+                            dataElement='pridec_climate_evi',
+                            delete_month_range=['202001', '202002'],
+                            dryRun = True)
+        
+def test_delete_historic_error_noPwd(dhis_url):
+    with pytest.raises(ValueError):
+        delete_historic_climate(base_url=dhis_url,
+                            user="user",
+                            pwd=None,
+                            orgUnit_ids=["O1wNJut8eci"],
+                            dataElement='pridec_climate_evi',
+                            delete_month_range=['202001', '202002'],
+                            dryRun = True)
+
+
